7ff1c61b1038da666affea270d2e90fae182d44d
FelixMR <felixmcneillrutter@gmail.com>
2024-08-04 15:42:54 +0100

    Filled in structure

--- src/components/SignupForm/Signupform.jsx
+++ src/components/SignupForm/Signupform.jsx
@@ -10,24 +10,36 @@ import * as authService from '../../services/authService';
 
 const SignupForm = () => {
 
+    const [formData, setformData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        verifyPassword: "",
+    })
+
     const handleChange = () => {
-        
+
     }
+
+    const {username, email, password, verifyPassword} = formData
 
     return (
         <main>
             <h1>Welcome to your SignupForm</h1>
             <form>
-                <label for="Input"></label>
-                <input id="" type="text" value={} name="" onChange={handleChange}></input>
-                <label for="Input"></label>
-                <input id="" type="text" value={} name="" onChange={handleChange}></input>
-                <label for="Input"></label>
-                <input id="" type="text" value={} name="" onChange={handleChange}></input>
-                <label for="Input"></label>
-                <input id="" type="text" value={} name="" onChange={handleChange}></input>
-                <label for="Input"></label>
-                <input id="" type="text" value={} name="" onChange={handleChange}></input>
+                <div>
+                <label for="usernameInput"></label>
+                <input id="usernameInput" type="text" value={username} name="username" onChange={handleChange}></input>
+                </div><div>
+                <label for="emailInput"></label>
+                <input id="emailInput" type="text" value={email} name="email" onChange={handleChange}></input>
+                </div><div>
+                <label for="passwordInput"></label>
+                <input id="passwordInput" type="text" value={password} name="password" onChange={handleChange}></input>
+                </div><div>
+                <label for="passwordVerifyInput"></label>
+                <input id="passwordVerifyInput" type="text" value={verifyPassword} name="verifyPassword" onChange={handleChange}></input>
+                </div>
                 <button></button>
             </form>
         </main>
