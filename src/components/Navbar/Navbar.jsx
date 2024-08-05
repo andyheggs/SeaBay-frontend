146a9437e79a9acc85e8d60a38461c8c65ff2a2b
FelixMR <felixmcneillrutter@gmail.com>
2024-08-05 12:42:41 +0100

    Added html ternary to the drop down menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,61 @@
 //---------------------------------------------React Imports-----------------------------------------------// 
-import { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useState, use } from 'react';
+import { useContext, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useState, use } from 'react'
 
 //-------------------------------------------Component Imports---------------------------------------------//
-import { AuthedUserContext } from '../../App';
+import { AuthedUserContext } from '../../App'
 import "./Navbar.css"
 
 //--------------------------------------------Service Imports----------------------------------------------//
 
 //--------------------------------------------Document Imports----------------------------------------------//
-    const burgerElement = document.getElementById("DropDownContainer")
+const burgerElement = document.getElementById("DropDownContainer")
 
 const Navbar = (props) => {
-    
+
     const user = useContext(AuthedUserContext)
 
     const [burgerToggle, setBurgerToggle] = useState("none")
 
     const toggleBurger = () => {
-        setBurgerToggle((burgerToggle === "none")? "block" : "none")
+        setBurgerToggle((burgerToggle === "none") ? "block" : "none")
     }
 
     return (
         <div>
             <p className="Marker" onClick={toggleBurger}>marker</p>
-            <div id="DropDownContainer" style={{display:burgerToggle}}>
-                <h3>Welcome to your Navbar</h3>
+            <div id="DropDownContainer" style={{ display: burgerToggle }}>
+                {(user)?
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li>
+                            <Link to="/hoots">Hoots</Link>
+                        </li>
+                        <li>
+                            <Link to="" onClick={props.handleSignout}>
+                                Sign Out
+                            </Link>
+                        </li>
+                    </ul>
+                </div>:
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/signin">Sign In</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">Sign Up</Link>
+                        </li>
+                    </ul>
+                </div>}
             </div>
         </div>
-    );
+    )
 
-    
-};    
+
+}
 
 
 
@@ -50,4 +73,4 @@ const Navbar = (props) => {
 
 //------------------------------------------------Exports--------------------------------------------------//
 
-export default Navbar;
+export default Navbar
