f1f23d53458018c304f8e408f7cc74359fe1b42b
FelixMR <felixmcneillrutter@gmail.com>
2024-08-06 14:40:32 +0100

    Added template for the offerService functions

---
+++ services/offerService.js
@@ -0,0 +1,67 @@
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
+
+const getOfferFromId = async (offerId) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
+// Maybe Obsolete 
+const getOffersFromListing = async (listingId) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
+// Maybe Obsolete 
+const getOffersFromUser = async (userId) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
+
+const assessOffer = async (rejected) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
+
+const createAnOffer = async (formData) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
+
+const editAnOffer = async (formData) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
+
+const deleteAnOffer = async (offerId) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
+
+
+export {getOfferFromId, getOffersFromListing, getOffersFromUser, assessOffer, createAnOffer, editAnOffer, deleteAnOffer}
