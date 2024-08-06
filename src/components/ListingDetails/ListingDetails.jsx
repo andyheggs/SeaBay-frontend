cf089f88e0932997ea7baff6efd0de83e496d6b7
Andy Heggs <Andy@Heggs.net>
2024-08-06 15:01:28 +0100

    updated-ListingDetails-imports_corrected

--- src/components/ListingDetails/ListingDetails.jsx
+++ src/components/ListingDetails/ListingDetails.jsx
@@ -1,5 +1,5 @@
 //---------------------------------------------React Imports-----------------------------------------------// 
-import React, {useEffect, useState } from 'react'
+import {useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom' 
 
 //-------------------------------------------Component Imports---------------------------------------------//
